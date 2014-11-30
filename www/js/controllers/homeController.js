30df7aa32de5e863706d42fbce0c7c1c31ea2ee3
vkostechko <viacheslav.kostechko@gmail.com>
2014-11-30 04:34:01 +0300

    added jCarousel

--- www/js/controllers/homeController.js
+++ www/js/controllers/homeController.js
@@ -5,6 +5,17 @@
 zovApp.controller('HomeController', ['$scope', function($scope) {
     $scope.greeting = 'Hello, World';
 
+    $scope.carouselImages = ['../resources/testImages/test-img-0.jpg',
+                            '../resources/testImages/test-img-1.jpg',
+                            '../resources/testImages/test-img-2.jpg',
+                            '../resources/testImages/test-img-3.jpg',
+                            '../resources/testImages/test-img-4.jpg',
+                            '../resources/testImages/test-img-5.jpg',
+                            '../resources/testImages/test-img-6.jpg',
+                            '../resources/testImages/test-img-7.jpg',
+                            '../resources/testImages/test-img-8.jpg',
+                            '../resources/testImages/test-img-9.jpg'];
+
     $scope.showGreetingAlert = function(){
         alert($scope.greeting);
     }
